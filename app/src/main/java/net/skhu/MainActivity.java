d37bb2a349ce8ad61a49cd18aeff16946953c1bc
Lee, Minji <leeminji0324@gamil.com>
2022-04-10 13:07:51 +0900

    최초 커밋

--- app/src/main/java/net/skhu/MainActivity.java
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -21,8 +21,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_signUp) {
             Intent intent = new Intent(this, SignupActivity.class);
@@ -32,12 +31,15 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, MemoActivity.class);
             startActivity(intent);
             return true;
-        }else if (id == R.id.action_buttons) {
+        } else if (id == R.id.action_buttons) {
             Intent intent = new Intent(this, ButtonActivity.class);
             startActivity(intent);
             return true;
+        } else if (id == R.id.action_checkboxes) {
+            Intent intent = new Intent(this, CheckboxesActivity.class);
+            startActivity(intent);
+            return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
