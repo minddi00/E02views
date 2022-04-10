656a23853140a16164f520d070b899e7a52629c3
Lee, Minji <leeminji0324@gamil.com>
2022-04-10 15:30:28 +0900

    SpinnersActivity

---
+++ app/src/main/java/net/skhu/SpinnersActivity.java
@@ -0,0 +1,14 @@
+package net.skhu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpinnersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinners);
+    }
+}
